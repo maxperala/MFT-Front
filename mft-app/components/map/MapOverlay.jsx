601457c42d16662b3ec670293cfd1f85b72dfaad
maxperala <max.perala@hotmail.fi>
2025-02-12 19:07:41 +0200

    rm useless comment

--- mft-app/components/map/MapOverlay.jsx
+++ mft-app/components/map/MapOverlay.jsx
@@ -1,9 +1,9 @@
 /**
  * Map Overlay Component
- * 
+ *
  * A translucent overlay component for the map view that displays arrival text
  * with fade animations and custom styling.
- * 
+ *
  * Features:
  * - Fade in/out animations
  * - Translucent background image
@@ -11,7 +11,7 @@
  * - Custom font integration
  * - Shadow effects for text
  * - Responsive full-screen layout
- * 
+ *
  * @component
  */
 import { colors } from "@/colors";
@@ -34,7 +34,6 @@ const MapOverlay = () => {
       entering={FadeIn}
       exiting={FadeOut}
     >
-      {/*I put it to repeat this if it has issues with other aspect ratio/resolution devices... I will fix if needed when testing the app*/}
       <Image
         source={{ uri: require("@/assets/images/map-overlay.png") }}
         width="100%"
